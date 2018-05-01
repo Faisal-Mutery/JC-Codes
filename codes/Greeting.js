b204e73a89ce7083d8dc594644847085a5090af4
Faisal-Mutery <38871091+Faisal-Mutery@users.noreply.github.com>
2018-05-01 03:02:43 +0300

    Update Greeting.js

--- codes/Greeting.js
+++ codes/Greeting.js
@@ -3,7 +3,19 @@ module.exports.get = function ( options ){
 	if( this.got == true ) return;
 	this.got = true;
 	return [ function ( args ){
-		
+		var dat = JSON.parse("{}");
+                function forEachObject(obj, func) {
+                Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
+                 }
+             var guild;
+             while (!guild)
+guild = member.guild.find("name", member.guild.name.forEach())
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        })
+    })
 		let member = args[0]
 		
 		if( options == undefined || options.channel == undefined ) return console.error( '[ Error JC-Codes ]: You need to put all arguments correct' );
@@ -11,10 +23,21 @@ module.exports.get = function ( options ){
 		if( options.message == undefined ) {
 			return channel.send( 'Welcome ' + member + '!' );
 		} else {
-			var message = options.message.replace('[user]',member);
-			return channel.send( message );
+			var message = options.message.replace('[user]',member).replace('[inviter]',Invite.inviter).replace('[InviteCode]',Invite.code);
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", member.guild.name)
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+			return channel.send( message ).catch(console.error);
+                }
+            dat[Inv] = Invite.uses;
+        })
+    })
 		}
 		
 	}, "guildMemberAdd" ];
 }
-
